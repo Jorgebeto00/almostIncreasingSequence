58dabfe92a01c268d241b86c159759b3f1a6d1b9
Jorge R <jorgebeto00@gmail.com>
2020-10-03 04:30:14 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+function almostIncreasingSequence(sequence) {
+	let determine;
+	let seq = sequence;
+	let limiter = 0;
+	//let random = Math.floor(Math.random()*seq.length);
+	//seq.splice(random, 1);
+	//console.log(random);
+	//console.log(seq);
+	
+	while(limiter < 2){
+		for(i = 0; i < seq.length; i++){
+			if (seq[i+1] >= seq[i] && seq[i+2] <= seq[i+1] && seq[i] < seq.length && seq[i] < seq.length){
+				seq.splice(seq[i+2], 1);
+				limiter += 1;
+				console.log(limiter);
+				console.log(seq);
+				//determine = true;
+			}/*else{
+				determine = false;
+			}*/
+		}
+	}
+	if(limiter === 1){
+		determine = true;
+	}else{
+		determine = false;
+	}
+	
+	return determine;
+}
+
+
+almostIncreasingSequence([1, 3, 2]); 
+//true
+/*[10, 1, 2, 3, 4, 5]true*/
